905f9d37be29681847d38ae04b40c1a6616a0819
Steve Benjamins <steve@stevebenjamins.com>
2012-03-20 20:07:11 -0500

    Got rid of shitty migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320012832) do
+ActiveRecord::Schema.define(:version => 20120320012900) do
 
   create_table "elements", :force => true do |t|
     t.string   "type"
